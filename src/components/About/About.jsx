1244a7f6d6c8409e6a1354ae70360a9c33e3cd4f
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-04 18:19:04 +0530

    added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import './about.css'
+import me from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <section id="about">
+            <h5>Get to know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={me} alt="About Image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+
+                    <div className="about__cards">
+
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>1+ Years Working</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>100+ Worlwide</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>20+ Completed</small>
+                        </article>
+
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, modi repellat assumenda minima placeat explicabo iste corrupti ipsa consectetur aperiam praesentium blanditiis quae laboriosam neque sed exercitationem nemo magnam et?
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
