189cdf473fd199804a7f70042dffcc0673e349c0
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-06 22:09:13 +0530

    added

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import { CgWebsite } from 'react-icons/cg';
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="www.linkedin.com/in/harsh-sangwan2003" target='_blank'><BsLinkedin /></a>
+            <a href="https://www.linkedin.com/in/harsh-sangwan2003" target='_blank'><BsLinkedin /></a>
             <a href="https://github.com/harsh-sangwan2003" target='_blank'><FaGithub /></a>
             <a href="https://drive.google.com/file/d/19yFconYVDiCDNtjIc3Ln-o-nM30jO8xU/view?usp=sharing" target='_blank'><CgWebsite /></a>
         </div>
