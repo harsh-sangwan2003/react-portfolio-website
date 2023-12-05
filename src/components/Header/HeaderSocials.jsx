93f877064853c4f8e01de49de1f59bc26ea05026
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-05 20:55:53 +0530

    added

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
         <div className='header__socials'>
             <a href="https://www.linkedin.com/in/harsh-sangwan2003" target='_blank'><BsLinkedin /></a>
             <a href="https://github.com/harsh-sangwan2003" target='_blank'><FaGithub /></a>
-            <a href="https://drive.google.com/file/d/19TxUOlwFftpzdlz2rBHGRUgaZXsLxW9_/view?usp=sharing" target='_blank'><CgWebsite /></a>
+            <a href="https://drive.google.com/file/d/17syu2PlXLG5RXU2UVm67_XKPaatTkyWd/view?usp=sharing" target='_blank'><CgWebsite /></a>
         </div>
     )
 }
